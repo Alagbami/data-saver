7488ebd2ed582ac2be2c07e4b690bd74f8819201
Adeleke <adelekeabiodun1996@gmail.com>
2024-11-24 19:58:58 +0100

    web title change

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: "StoreIt",
-  description: "StoreIt - The only storage solution you need.",
+  title: "Data Saver",
+  description: "SaverIt - The only storage solution you need.",
 };
 
 export default function RootLayout({
